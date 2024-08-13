616c38c85d75dc3df9889a20d583c965aa9527e2
Valeriya Sapega <124445060+valeriya-sapega@users.noreply.github.com>
2024-08-13 21:55:28 +0100

    refactored the code

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 #!/usr/bin/env node
 
 import fs from 'fs';
-import { countBytes, countLines, countWords } from './utils.js';
 
-const [command, file] = process.argv.slice(2);
+const getData = (file) => {
+    const data = fs.readFileSync(file, 'utf8');
+    return {
+        '-c': fs.statSync(file).size,
+        '-l': data.split('\n').length,
+        '-w': data.trim().split(/\s+/).length,
+        '-m': data.length,
+    };
+};
 
-//console.log(countBytes(args[1]), args[1]);
+const handleCommand = (command, file) => {
+    const output = getData(file)[command];
+    console.log(output, file);
+};
 
-//console.log(fs.statSync(file));
-
-countWords(file);
+if (process.argv.length === 3 && process.argv[2].includes('.txt')) {
+    const data = getData(process.argv[2]);
+    console.log(data['-c'], data['-l'], data['-w'], process.argv[2]);
+} else if (process.argv.length === 4 && process.argv[3].includes('.txt')) {
+    const [command, file] = process.argv.slice(2);
+    handleCommand(command, file);
+} else {
+    console.log(
+        'Coding Challenge wc – word, line, character, and byte count\nUse ccwc -c|-l|-w|-m [filePath]'
+    );
+}
