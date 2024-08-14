f1a5d1a7752b75537f38f1f998ee3d38444845b3
Valeriya Sapega <124445060+valeriya-sapega@users.noreply.github.com>
2024-08-14 19:19:10 +0100

    Refactored the code

--- index.js
+++ index.js
@@ -2,29 +2,68 @@
 
 import fs from 'fs';
 
-const getData = (file) => {
-    const data = fs.readFileSync(file, 'utf8');
+const getData = (data) => {
     return {
-        '-c': fs.statSync(file).size,
+        '-c': Buffer.byteLength(data),
         '-l': data.split('\n').length,
         '-w': data.trim().split(/\s+/).length,
         '-m': data.length,
     };
 };
 
-const handleCommand = (command, file) => {
-    const output = getData(file)[command];
-    console.log(output, file);
+const commands = ['-c', '-l', '-w', '-m'];
+
+const processFile = () => {
+    const args = process.argv.slice(2);
+
+    if (args.length === 0 || args.length > 2) {
+        console.log(
+            'Coding Challenge wc – word, line, character, and byte count\nUse ccwc -c|-l|-w|-m [filePath]'
+        );
+        return;
+    }
+
+    if (args.length === 1) {
+        if (fs.existsSync(args[0])) {
+            const fileData = fs.readFileSync(args[0], 'utf8');
+            const data = getData(fileData);
+            console.log(data['-c'], data['-l'], data['-w'], args[0]);
+        } else if (commands.includes(args[0])) {
+            let data = '';
+            process.stdin.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            process.stdin.on('end', () => {
+                const output = getData(data)[args[0]];
+                console.log(output);
+            });
+        } else {
+            console.log('Invalid command or file path.');
+        }
+    }
+
+    if (args.length === 2) {
+        const [command, file] = args;
+
+        if (!commands.includes(command)) {
+            console.log('Invalid command.');
+            return;
+        }
+
+        if (!fs.existsSync(file)) {
+            console.log('File not found.');
+            return;
+        }
+
+        const fileData = fs.readFileSync(file, 'utf8');
+        const output = getData(fileData)[command];
+        console.log(output, file);
+    }
 };
 
-if (process.argv.length === 3 && process.argv[2].includes('.txt')) {
-    const data = getData(process.argv[2]);
-    console.log(data['-c'], data['-l'], data['-w'], process.argv[2]);
-} else if (process.argv.length === 4 && process.argv[3].includes('.txt')) {
-    const [command, file] = process.argv.slice(2);
-    handleCommand(command, file);
-} else {
-    console.log(
-        'Coding Challenge wc – word, line, character, and byte count\nUse ccwc -c|-l|-w|-m [filePath]'
-    );
+try {
+    processFile();
+} catch (error) {
+    console.log(error.message);
 }
